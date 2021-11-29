b53739a82ecfd50023634b7b0c45c4212fb001e7
Jo Wagner <jowagner@limago.de>
2021-11-29 14:31:45 +0100

    Initial commit

---
+++ webapp/src/test/java/de/db/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package de.db.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
