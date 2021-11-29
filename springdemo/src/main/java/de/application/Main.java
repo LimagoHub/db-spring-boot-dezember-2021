a25feda7c75e6bd8b55d95b7e63694540de8f352
Jo Wagner <jowagner@limago.de>
2021-11-29 13:20:18 +0100

    Annotations

--- springdemo/src/main/java/de/application/Main.java
+++ springdemo/src/main/java/de/application/Main.java
@@ -2,13 +2,15 @@ package de.application;
 
 import de.application.demo.Hello;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
 
 
-
+@ComponentScan("de.application")
 public class Main {
 
 	/**
@@ -16,7 +18,8 @@ public class Main {
 	 */
 
 	public static void main(String[] args) {
-		final AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+		//final AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+		final AbstractApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
 		context.registerShutdownHook();
 
 		Hello hello = context.getBean(Hello.class);
